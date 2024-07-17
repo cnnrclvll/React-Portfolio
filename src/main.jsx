ef896bab98738149f04cb7fdbf1e3896ef7e422f
Connor Colvill <cnnrclvll@gmail.com>
2024-07-17 13:57:25 -0400

    fixing for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
+import App from './components/App';
 import Bio from './components/Bio';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
