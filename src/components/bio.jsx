f4595ba5c5886ddf42b38cbefd7d6546c0044dcc
Connor Colvill <cnnrclvll@gmail.com>
2024-07-17 14:02:23 -0400

    syntax fix

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import portrait from '../assets/seq.png';
 
 const Bio = () => {
@@ -8,7 +7,7 @@ const Bio = () => {
       <img src={portrait} alt="Your Portrait"/>
       <p>Connor Colvill is an early developer from North Carolina with a unique twist.  An extensive background in the arts gives him a creative edge as a developer.  For over a decade he has created portfolios across multiple media, including but not limited to: etching, lithography, installation, sculpture, and drawing.  During this time, Colvill collaborated both creatively and technically with numerous professional artists and participated in a variety of special projects internationally.  Through years of honing craft he has established a detail-oriented standard for his work, which he seeks to apply to his novel career as a developer.
 
-For the past five years Colvill has been teaching for East Carolina University's School of Art & Design.  Teaching satisfies his love for creative communities and his desire to help others achieve their goals.  Colvill says teaching preserves his passion for learning and in many ways contributed to his choice in becoming a developer.</p>
+For the past five years Colvill has been teaching for East Carolina University School of Art & Design.  Teaching satisfies his love for creative communities and his desire to help others achieve their goals.  Colvill says teaching preserves his passion for learning and in many ways contributed to his choice in becoming a developer.</p>
     </div>
 
   );
