b8c6360ec1922df104233b65331b6d82edaf8445
Connor Colvill <cnnrclvll@gmail.com>
2024-07-17 13:51:22 -0400

    router DOM applied

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Portfolio/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
 })
